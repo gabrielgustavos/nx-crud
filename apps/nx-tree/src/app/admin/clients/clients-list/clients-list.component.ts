9229e0c5f3fc6422fd6a6f7418f7d1f14bcc4cfd
gabriel.silva <gabriel.silva@treeinova.com.br>
2023-09-05 14:26:12 -0300

    Implementação de novas colunas e metodos.

--- apps/nx-tree/src/app/admin/clients/clients-list/clients-list.component.ts
+++ apps/nx-tree/src/app/admin/clients/clients-list/clients-list.component.ts
@@ -1,9 +1,9 @@
-import { ElementRef, ViewChild, inject } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
 import { ModalRemoverComponent, ModalService } from '@nx-org/components';
 import { ClientModel, ResponseModel } from '@nx-org/interfaces';
 import { ClientService, ExcelService } from '@nx-org/services';
 import { filter, take } from 'rxjs';
+import { CadEditClientsComponent } from './cad-edit-clients/cad-edit-clients.component';
 
 @Component({
   selector: 'nx-org-clients-list',
@@ -16,7 +16,7 @@ export class ClientsListComponent implements OnInit {
   dataSource: any
   @ViewChild('TABLE', { static: true }) table: ElementRef;
 
-  displayedColumns: string[] = ['nome', 'email', 'telefone', 'funcao', 'linguagem', 'acoes'];
+  displayedColumns: string[] = ['nome', 'email', 'telefone', 'cpf', 'status', 'acoes'];
   private clientService = inject(ClientService)
   private excelService = inject(ExcelService)
   private modalService = inject(ModalService)
@@ -51,6 +51,26 @@ export class ClientsListComponent implements OnInit {
       )
       .subscribe(data => {
         return data
+      });
+  }
+
+  cadastrarCliente(data: ClientModel[]) {
+    const modal = this.modalService.open(CadEditClientsComponent, {
+      width: '50rem',
+      clickOutside: true,
+      data
+    });
+
+    modal
+      .afterClosed()
+      .pipe(
+        take(1),
+        filter((data) => data != false)
+      )
+      .subscribe(data => {
+        if (data) {
+          this.getClients()
+        }
       });
   }
 }
--- apps/nx-tree/src/app/admin/clients/clients.module.ts
+++ apps/nx-tree/src/app/admin/clients/clients.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ClientsComponent } from './clients.component';
@@ -5,12 +6,15 @@ import { ClientsRouting } from './clients.routing';
 import { ClientsListComponent } from './clients-list/clients-list.component';
 import { CdkTableModule } from '@angular/cdk/table';
 import { ClientService, ExcelService } from '@nx-org/services';
-import { ModalService, TooltipModule } from '@nx-org/components';
+import { AlertService, ButtonModule, InputModule, ModalService, ToggleModule, TooltipModule } from '@nx-org/components';
+import { CadEditClientsComponent } from './clients-list/cad-edit-clients/cad-edit-clients.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CdkTableModule, TooltipModule, ClientsRouting],
+  imports: [CdkTableModule, CommonModule, ReactiveFormsModule, TooltipModule, InputModule, ToggleModule, NgSelectModule, ButtonModule, ClientsRouting],
   exports: [],
-  declarations: [ClientsComponent, ClientsListComponent],
-  providers: [ClientService, ExcelService, ModalService],
+  declarations: [ClientsComponent, ClientsListComponent, CadEditClientsComponent],
+  providers: [ClientService, ExcelService, ModalService, AlertService],
 })
 export class ClientsModule { }
